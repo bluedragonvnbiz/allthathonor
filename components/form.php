53d76cfdd7da3bfc26b6d952075ae0766fba8da9
tanpv01 <tan@bluedragonvn.com>
2025-08-15 17:42:02 +0700

    UI admin component final

--- components/form.php
+++ components/form.php
@@ -236,3 +236,83 @@
 		</div>
 	</div>
 </form>
+
+<form class="card">
+	<div class="card-header d-flex align-items-center justify-content-between">
+		<strong class="title">혜택 정보</strong>
+		<button class="btn btn-primary lh-1" type="submit">저장</button>
+	</div>
+	<div class="card-body p-0">
+		<div class="section-box">
+			<nav class="nav-tab-btn d-flex align-items-center">
+				<button class="btn p-0 border-0 active" type="button">트래블케어</button>
+				<button class="btn p-0 border-0" type="button">라이프스타일</button>
+				<button class="btn p-0 border-0" type="button">스페셜베네핏</button>
+				<button class="btn p-0 border-0" type="button">웰컴기프트</button>
+			</nav>
+		</div>
+		<div class="section-box">			
+			<table class="table table-hover mb-0 table-input">
+				<thead>
+					<tr>
+						<th style="width: 180px;">제공혜택</th>
+						<th>설명</th>
+						<th style="width: 80px;">사용여부</th> 
+						<th style="width: 80px;">
+							<div class="d-flex align-items-center justify-content-center">
+								<span>요약혜택</span>
+								<div class="pv-tooltip">
+									<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+										<circle cx="10.0003" cy="10.0003" r="7.5" stroke="#A0A5B5" stroke-width="1.66667"/>
+										<circle cx="9.99967" cy="13.1252" r="1.04167" fill="#A0A5B5"/>
+										<path d="M7.91699 7.91691C7.91699 6.99644 8.84973 6.25024 10.0003 6.25024C11.1509 6.25024 12.0837 6.99644 12.0837 7.91691C12.0837 8.72371 11.3671 9.39662 10.4153 9.55051C10.1881 9.58725 10.0003 9.77013 10.0003 10.0002V10.4169" stroke="#A0A5B5" stroke-width="1.66667" stroke-linecap="round"/>
+									</svg>
+									<div class="tooltip-content">
+										멤버십마다 사용 중인 혜택중에서 최대<br>3개의 요약혜택만 선택할 수 있으며, <br>이 3개의 요약혜택은 멤버십<br> 페이지의 섹션 1에 표시됩니다.
+									</div>
+								</div>
+							</div>
+						</th>				      
+					</tr>
+				</thead>
+				<tbody>
+				<?php  
+				$arr = [
+					["공항 의전 서비스"],
+					["VIP 쇼퍼 서비스"],
+					["공항 의전 서비스"],
+					["24시간 해외 트래블 케어"],
+					["전용기 예약 지원"],
+					["인천공항 라운지 이용권"],
+				];
+				foreach ($arr as $key => $value) { ?>
+					<tr>
+						<td><?= $value[0] ?></td>
+						<td style="text-align:left;">내용에 대한 설명입니다.</td>
+						<td>
+							<div class="form-check checkbox justify-content-center">
+								<input class="form-check-input <?= rand(0, 1) ? 'black' : '' ?>" type="checkbox" value="" name="" <?= rand(0, 1) ? 'disabled' : '' ?> <?= rand(0, 1) ? 'checked' : '' ?>>
+							</div>
+						</td>
+						<td>
+							<div class="form-check checkbox justify-content-center">
+								<input class="form-check-input <?= rand(0, 1) ? 'black' : '' ?>" type="checkbox" value="" name="" <?= rand(0, 1) ? 'disabled' : '' ?> <?= rand(0, 1) ? 'checked' : '' ?>>
+							</div>
+						</td>
+					</tr>
+				<?php
+				}
+				?>
+				</tbody>
+			</table>			
+		</div>
+		<div class="section-box border-0">
+			<div class="d-flex column-gap-30">
+				<label class="form-label">이용안내</label>
+				<textarea class="form-control" placeholder="" style="height: 126px;">이용안내에 대한 설명입니다.이용안내에 대한 설명입니다.이용안내에 대한 설명입니다.이용안내에 대한 설명입니다.이용안내에 대한 설명입니다.이용안내에 대한 설명입니다.이용안내에 대한 설명입니다.이용안내에 대한 설명입니다.이용안내에 대한 설명입니다.
+이용안내에 대한 설명입니다.이용안내에 대한 설명입니다.이용안내에 대한 설명입니다.이용안내에 대한 설명입니다.이용안내에 대한 설명입니다.
+이용안내에 대한 설명입니다.이용안내에 대한 설명입니다.이용안내에 대한 설명입니다.이용안내에 대한 설명입니다.이용안내에 대한 설명입니다.</textarea>
+			</div>
+		</div>
+	</div>
+</form>
